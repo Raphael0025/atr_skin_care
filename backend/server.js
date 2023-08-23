cb5a6e38c0719dc31d5a4f2379463b7c59cfe52b
Raphael Isla <rpbisla0025@gmail.com>
2023-08-23 18:06:05 +0800

    backend

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,15 @@
+require('dotenv').config()
+
 const express = require('express')
 
 //init express app
 const app = express()
+
+//middleware
+app.use((req, res, next) => {
+    console.log(req.path, req.method)
+    next()
+})
 
 // route handler
 app.get('/', (req, res) => {
@@ -9,6 +17,7 @@ app.get('/', (req, res) => {
 })
 
 // listen for requests
-app.listen(4000, () => {
-    console.log('Listening on port 4000')
+app.listen(process.env.PORT, () => {
+    console.log('Listening on port', process.env.PORT)
 })
+
